1b55c67bacd03b0fa9488217b992dd0030d4e222
NathanPletz <pletznathan8@gmail.com>
2023-03-26 11:32:19 -0400

    Initial Commit

---
+++ src/main/java/net/lynstrum/emilysmod/EmilysModClient.java
@@ -0,0 +1,10 @@
+package net.lynstrum.emilysmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EmilysModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
